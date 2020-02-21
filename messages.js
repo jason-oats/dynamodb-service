6c52844922ddf0fe07629786116a3a965347ba9c
Jason Oats <bigirish1985@gmail.com>
2020-02-20 21:56:35 -0600

    updates yml with filename-queue info, handles test message sent by aws

--- messages.js
+++ messages.js
@@ -2,35 +2,32 @@ const aws = require('aws-sdk');
 const sqs = new aws.SQS();
 
 module.exports.handleInputNotification = (event, _context, callback) => {
-    const messageObj = JSON.parse(event.Records[0].body).Message;
-    const filename = JSON.parse(messageObj).Records[0].s3.object.key;
-    console.log(filename);
+    const messageObj = JSON.parse(((event.Records || [])[0] || {}).body || {}).Message;
+    const filename = ((((JSON.parse(messageObj).Records || [])[0] || {}).s3 || {}).object || {}).key;
 
     const params = {
         QueueUrl: process.env.FILENAME_QUEUE_URL,
         MessageBody: filename
     };
 
-    sqs.sendMessage(
-        params,
-        (err, response) => {
-            if (err) {
-                console.log(err);
-                callback(err, null);
+    if (filename) {
+        sqs.sendMessage(
+            params,
+            (err, response) => {
+                if (err) {
+                    console.log(err);
+                    callback(err, null);
+                }
+                console.log('Messages received');
+                callback(null, response);
             }
-            console.log('Messages received');
-            console.log(response);
-            callback(null, response);
-        }
-    );
+        );
+    } else {
+        callback(null, 'Message not triggered by put event');
+    }
 }
 
 module.exports.processFiles = (event, _context, callback) => {
-    // const params = {
-    //     QueueUrl: process.env.FILENMAE_QUEUE_URL,
-    //     MaxNumberOfMessages: 10
-    // }
-
     console.log(event.Records[0].body);
     callback(null, 'Success');
 }
